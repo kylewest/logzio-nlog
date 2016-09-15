2a29f145739371532d1629d9de96f43174d61a58
Kyle West <kswest@gmail.com>
2016-09-15 13:46:27 +0000

    move files into solution and project

---
+++ NLog.LogzIO/LogzIOLogEventInfo.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using Newtonsoft.Json;
+
+namespace NLog.LogzIO
+{
+    internal class LogzIOLogEventInfo
+    {
+        public LogzIOLogEventInfo(string pToken, string pMessage, DateTime pLogTimestamp, string pHost, LogLevel pLevel, Exception pException)
+        {
+            Token = pToken;
+            Message = pMessage;
+            LogTimestamp = pLogTimestamp;
+            Host = pHost;
+            Level = pLevel.ToString();
+            Exception = pException;
+        }
+        
+        public string Token { get; set; }
+        public string Message { get; set; }
+        public DateTime LogTimestamp { get; set; }
+        public string Type { get; set; }
+        public string Host { get; set; }
+        public string Level { get; set; }
+        public Exception Exception { get; set; }
+        [JsonExtensionData]
+        public IDictionary<object, object> Details { get; set; }
+    }
+}
--- NLog.LogzIO/LogzIOTarget.cs
+++ NLog.LogzIO/LogzIOTarget.cs
@@ -5,37 +5,12 @@ using System.Net;
 using System.Net.Sockets;
 using System.Text;
 using Newtonsoft.Json;
-using Newtonsoft.Json.Serialization;
-using NLog;
 using NLog.Common;
 using NLog.Config;
 using NLog.Targets;
 
-namespace Logging.NLog.Targets.Redis
+namespace NLog.LogzIO
 {
-    internal class LogzIOLogEventInfo
-    {
-        public LogzIOLogEventInfo(string pToken, string pMessage, DateTime pLogTimestamp, string pHost, LogLevel pLevel, Exception pException)
-        {
-            Token = pToken;
-            Message = pMessage;
-            LogTimestamp = pLogTimestamp;
-            Host = pHost;
-            Level = pLevel.ToString();
-            Exception = pException;
-        }
-        
-        public string Token { get; set; }
-        public string Message { get; set; }
-        public DateTime LogTimestamp { get; set; }
-        public string Type { get; set; }
-        public string Host { get; set; }
-        public string Level { get; set; }
-        public Exception Exception { get; set; }
-        [JsonExtensionData]
-        public IDictionary<object, object> Details { get; set; }
-    }
-
     [Target("LogzIO")]
     public class LogzIOTarget : TargetWithLayout
     {
@@ -113,7 +88,7 @@ namespace Logging.NLog.Targets.Redis
         {
             try
             {
-                var logEventProperties = logEvent.CleanCallerData().Properties;
+                var logEventProperties = logEvent.Properties;
 
                 var fieldNameToUseAsDataType = GetFieldNameToUseAsDateType(logEventProperties);
                 
@@ -150,13 +125,4 @@ namespace Logging.NLog.Targets.Redis
                     .FirstOrDefault();
         }
     }
-
-    public class LowercaseContractResolver : DefaultContractResolver
-    {
-        protected override string ResolvePropertyName(string propertyName)
-        {
-            return propertyName.ToLower();
-        }
-    }
-
 }
